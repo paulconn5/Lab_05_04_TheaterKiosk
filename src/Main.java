fff086e9eec8c3b6207b062f4f05ef0a69e928ab
Paul Connolly <paulconn5@gmail.com>
2022-09-25 23:15:06 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 30;
+        System.out.println("Welcome to the theatre. What is your age?");
+
+        if (age >= 21) {
+            System.out.println("You get a wrist band!");
+        }
+        else {
+            System.out.println("No wristband for you.");
+        }
     }
 }
